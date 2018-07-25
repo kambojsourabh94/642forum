75cefc5c38cd23ba5f6781047a344d315c6467e6
skamboj <skamboj@mail.sfsu.edu>
2018-07-25 11:59:08 -0700

    set up the server

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const bodyParser = require('body-parser')
+
+const app = express()
+
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+app.set('view engine', 'ejs')
+
+
+app.get('/', (req, res)=> {
+
+	 res.render('register')
+})
+
+
+app.post('/register', (req, res)=> {
+
+	  console.log(req.body.firstname)
+})
+app.listen(3000 || process.env.PORT, () => {
+	  console.log('server running ')
+})
